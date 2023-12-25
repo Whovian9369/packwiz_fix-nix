985b2018635dd6b16eca889398d58f3e30a2108b
Whovian9369 <Whovian9369@gmail.com>
2023-12-25 01:40:32 -0500

    Rearranged the formatting of `nix/default.nix` a little.

--- nix/default.nix
+++ nix/default.nix
@@ -2,37 +2,36 @@ let
   # Import nixpkgs if needed
   pkgs = import <nixpkgs> {};
 in
-  {
-    lib ? pkgs.lib,
-    buildGoModule ? pkgs.buildGoModule,
-    fetchFromGitHub ? pkgs.fetchFromGitHub,
-    installShellFiles ? pkgs.installShellFiles,
+  { lib,
+    buildGoModule,
+    fetchFromGitHub,
+    installShellFiles,
     # version and vendorSha256 should be specified by the caller
     version ? "latest",
     vendorSha256,
   }:
-    buildGoModule rec {
-      pname = "packwiz";
-      inherit version vendorSha256;
+  buildGoModule {
+    pname = "packwiz";
+    inherit version vendorSha256;
 
-      src = ./..;
+    src = ./..;
 
-      nativeBuildInputs = [
-        installShellFiles
-      ];
+    nativeBuildInputs = [
+      installShellFiles
+    ];
 
-      # Install shell completions
-      postInstall = ''
-        installShellCompletion --cmd packwiz \
-          --bash <($out/bin/packwiz completion bash) \
-          --fish <($out/bin/packwiz completion fish) \
-          --zsh <($out/bin/packwiz completion zsh)
-      '';
+    # Install shell completions
+    postInstall = ''
+      installShellCompletion --cmd packwiz \
+        --bash <($out/bin/packwiz completion bash) \
+        --fish <($out/bin/packwiz completion fish) \
+        --zsh <($out/bin/packwiz completion zsh)
+    '';
 
-      meta = with lib; {
-        description = "A command line tool for editing and distributing Minecraft modpacks, using a git-friendly TOML format";
-        homepage = "https://packwiz.infra.link/";
-        license = licenses.mit;
-        mainProgram = "packwiz";
-      };
-    }
+    meta = {
+      description = "A command line tool for editing and distributing Minecraft modpacks, using a git-friendly TOML format";
+      homepage = "https://packwiz.infra.link/";
+      license = lib.licenses.mit;
+      mainProgram = "packwiz";
+    };
+  }
